58ac9810924aec2a149cabe21226bf07411cd462
Jon Wu <jonathan.wu124@gmail.com>
2024-04-10 12:15:29 -0400

    initial code for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from '../src/components/Header/Header'
+import LandingPage from '../src/pages/LandingPage/LandingPage'
 
 import '../styles/partials/_typography.scss'
 
