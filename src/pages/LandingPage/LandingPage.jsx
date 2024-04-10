5598909cc5d70b6d5a8990210a90ca9954cef0b2
Jon Wu <jonathan.wu124@gmail.com>
2024-04-10 17:19:31 -0400

    updated video, added new info component

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import LandingPageBanner from '../../components/LandingPageBanner/LandingPageBanner'
-import landingVideo from '../../assets/videos/landing_page.mp4'
-import './LandingPage.scss'
+import LandingPageBanner from "../../components/LandingPageBanner/LandingPageBanner";
+import InfoBanner from "../../components/InfoBanner/InfoBanner"
+import landingVideo from "../../assets/videos/landing_page_video.mp4";
+import globe from "../../assets/images/globe.jpg"
+import "./LandingPage.scss";
 
 const LandingPage = () => {
   return (
     <div>
-        <LandingPageBanner 
-            video={landingVideo}
-            title="Adventure Awaits: Let's Build Memories Beyond Imagination"
-            />
-        <div>
-        <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante nec risus fermentum pharetra. Quisque ut volutpat libero. Nullam tincidunt, leo at tempus fermentum, libero felis cursus mi, eu vehicula magna eros id justo. In hac habitasse platea dictumst. Sed ac dapibus odio. Nullam in quam ac purus fermentum semper. Vivamus in diam eu libero tincidunt suscipit eget et odio. Sed accumsan quam vitae neque rutrum, a suscipit magna varius. Maecenas nec tortor sed libero condimentum vehicula. Curabitur dapibus justo vel libero malesuada efficitur. Donec vitae justo ac justo interdum semper. Curabitur vel sapien ac magna sodales sollicitudin. Nam id risus id lorem pellentesque auctor. Maecenas et diam vitae eros ultricies convallis eget id metus. Sed vel lorem non risus tincidunt congue eget id libero. Nunc lobortis libero nec faucibus tincidunt.</p>    
+      <LandingPageBanner
+        video={landingVideo}
+        title="Adventure Awaits: Let's Build Memories Beyond Imagination"
+      />
+      <InfoBanner 
+      header="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      value1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante
+      nec risus fermentum pharetra. Quisque ut volutpat libero."
+      value2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante
+      nec risus fermentum pharetra. Quisque ut volutpat libero."
+      value3="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante
+      nec risus fermentum pharetra. Quisque ut volutpat libero."
+      image={globe}
+/>
+
+      <div>
+        <p className="text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac ante
+          nec risus fermentum pharetra. Quisque ut volutpat libero. Nullam
+          tincidunt, leo at tempus fermentum, libero felis cursus mi, eu
+          vehicula magna eros id justo. In hac habitasse platea dictumst. Sed ac
+          dapibus odio. Nullam in quam ac purus fermentum semper. Vivamus in
+          diam eu libero tincidunt suscipit eget et odio. Sed accumsan quam
+          vitae neque rutrum, a suscipit magna varius. Maecenas nec tortor sed
+          libero condimentum vehicula. Curabitur dapibus justo vel libero
+          malesuada efficitur. Donec vitae justo ac justo interdum semper.
+          Curabitur vel sapien ac magna sodales sollicitudin. Nam id risus id
+          lorem pellentesque auctor. Maecenas et diam vitae eros ultricies
+          convallis eget id metus. Sed vel lorem non risus tincidunt congue eget
+          id libero. Nunc lobortis libero nec faucibus tincidunt.
+        </p>
+      </div>
     </div>
+  );
+};
 
-            </div>
-            
-
-  )
-}
-
-export default LandingPage
+export default LandingPage;
