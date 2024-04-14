e96af2354140ce19a4b7398c116ff412ad06fedf
Jon Wu <jonathan.wu124@gmail.com>
2024-04-14 07:48:46 -0400

    styled the homepage

--- src/components/Homepage/ChallengesBanner/ChallengesBanner.jsx
+++ src/components/Homepage/ChallengesBanner/ChallengesBanner.jsx
@@ -4,16 +4,15 @@ import { Link } from 'react-router-dom'
 const ChallengesBanner = (props) => {
   return (
     <div className="challenge">
-        <div className="challenge__cards">
+        <div className="challenge__details">
             <img className="challenge__image" src={props.image} />
-            <div className="challenge__details">
-                <p className="challenge__title">{props.title}</p>
+            <p className="challenge__title">{props.title}</p>
+            </div>
+            <div className="challenge__details-bottom">
                 <p className="challenge__description">{props.description}</p>
                 <Link className="challenge__button" to={props.url}>{props.buttonlabel}</Link>
-            </div>
         </div>
-    
-    
+
     </div>
   )
 }
